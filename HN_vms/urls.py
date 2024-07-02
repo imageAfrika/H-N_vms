1552a03eb5b2c2012d2a0e281e911ea3ca562d4e
Tigerfrake <you@example.com>
2024-07-02 16:59:38 +0300

    test change

--- HN_vms/urls.py
+++ HN_vms/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # Default admin urls
     path('admin/', admin.site.urls),
     
-    # First page 
+    # First page to open
     path('', RedirectView.as_view(url='/users/auth/login/', permanent=False)),
 
     # urlpatterns for users
